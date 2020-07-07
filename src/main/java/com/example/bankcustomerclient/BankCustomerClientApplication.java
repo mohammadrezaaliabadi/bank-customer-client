5ec5a6d16e49e34c33a8cf2ee04fb27bc318d5b8
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-07-07 04:51:40 +0430

    first commit

---
+++ src/main/java/com/example/bankcustomerclient/BankCustomerClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankcustomerclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankCustomerClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankCustomerClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankcustomerclient/BankCustomerClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankcustomerclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankCustomerClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
