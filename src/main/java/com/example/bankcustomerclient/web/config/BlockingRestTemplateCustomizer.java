cae353129873de2669c80505d4d9725cde4dae07
Mohammad Reza AliAbadi <mohammadrezaaliabadi9@gmail.com>
2020-07-07 06:23:43 +0430

    add external prop apache closes #7 add apache-req logging closes #6

--- src/main/java/com/example/bankcustomerclient/web/config/BlockingRestTemplateCustomizer.java
+++ src/main/java/com/example/bankcustomerclient/web/config/BlockingRestTemplateCustomizer.java
@@ -1,11 +1,11 @@
 package com.example.bankcustomerclient.web.config;
 
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultConnectionKeepAliveStrategy;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.elasticsearch.RestClientBuilderCustomizer;
 import org.springframework.boot.web.client.RestTemplateCustomizer;
 import org.springframework.http.client.ClientHttpRequestFactory;
@@ -15,16 +15,30 @@ import org.springframework.web.client.RestTemplate;
 
 @Component
 public class BlockingRestTemplateCustomizer implements RestTemplateCustomizer {
+    private final Integer maxTotalConnections;
+    private final Integer defaultMaxTotalConnetions;
+    private final Integer connectionRequestTimeout;
+    private final Integer socketTimeout;
+
+    public BlockingRestTemplateCustomizer(@Value("${bank.client.maxtotalconnections}") Integer maxTotalConnections,
+                                          @Value("${bank.client.defaultmaxtotalconnections}") Integer defaultMaxTotalConnetions,
+                                          @Value("${bank.client.connectionrequesttimeout}")Integer connectionRequestTimeout,
+                                          @Value("${bank.client.sockettimeout}")Integer socketTimeout) {
+        this.maxTotalConnections = maxTotalConnections;
+        this.defaultMaxTotalConnetions = defaultMaxTotalConnetions;
+        this.connectionRequestTimeout = connectionRequestTimeout;
+        this.socketTimeout = socketTimeout;
+    }
 
     public ClientHttpRequestFactory clientHttpRequestFactory(){
         PoolingHttpClientConnectionManager clientConnectionManager = new PoolingHttpClientConnectionManager();
-        clientConnectionManager.setMaxTotal(100);
+        clientConnectionManager.setMaxTotal(maxTotalConnections);
 
-        clientConnectionManager.setDefaultMaxPerRoute(20);
+        clientConnectionManager.setDefaultMaxPerRoute(defaultMaxTotalConnetions);
 
         RequestConfig requestConfig = RequestConfig.
-                custom().setConnectionRequestTimeout(3000)
-                .setSocketTimeout(3000)
+                custom().setConnectionRequestTimeout(connectionRequestTimeout)
+                .setSocketTimeout(socketTimeout)
                 .build();
 
         CloseableHttpClient httpClient = HttpClients.custom()
